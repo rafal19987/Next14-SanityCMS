80fea740911bf22af8c4ff15133984a1129545d3
Rafixx <rafal987.1@o2.pl>
2024-10-28 11:23:09 +0100

    feat: update sanity structure files

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,5 +1,22 @@
 import { type SchemaTypeDefinition } from "sanity";
+import page from "@/sanity/schemaTypes/documents/page";
+import navigation from "@/sanity/schemaTypes/documents/navigation";
+import site from "@sanity/schemaTypes/documents/site";
+import video from "@/sanity/schemaTypes/objects/video";
+import service from "@/sanity/schemaTypes/objects/service";
+import components from "@/sanity/schemaTypes/modules";
+import seo from "@/sanity/schemaTypes/objects/seo";
+import button from "@/sanity/schemaTypes/objects/button";
+import portableText from "@/sanity/schemaTypes/objects/portableText";
+import paragraph from "@/sanity/schemaTypes/objects/paragraph";
+import headlineWithText from "@/sanity/schemaTypes/objects/headlineWithText";
+import testimonial from "@/sanity/schemaTypes/objects/testimonial";
+
+const documentTypes = [page, navigation, site];
+const singleTypes = [service, video, headlineWithText, testimonial];
+const Components = [components];
+const ui = [seo, button, portableText, paragraph];
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [...documentTypes, ...singleTypes, ...Components, ...ui],
 };
